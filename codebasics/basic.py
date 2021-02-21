dcde9d37f5adc5651e9c5047085f161ecc8da7dc
Nishank Parashar <nishankparasher@gmail.com>
2021-02-22 00:59:31 +0530

    Session2

--- codebasics/basic.py
+++ codebasics/basic.py
@@ -67,4 +67,78 @@ print(len(combine_list))
 
 print("soad" in combine_list) # lookup in list
 
-# https://www.youtube.com/watch?v=hNddJ3_hahk&list=PLeo1K3hjS3us_ELKYSj_Fth2tIEkdKXvV&index=10
+# if statement
+"""
+num=input("Enter a Number : ")
+if(int(num)%2 == 0):
+    print("Even")
+else:
+    print("Odd")"""
+
+print(4!=5)
+
+print(5>3 and 7<9)
+
+print(not(4==4))
+
+"""indian=["samosa","kachori","golgappe"]
+south_indian=["idli","dosa","uttapam"]
+chinese=["momos","noodles","manchurian"]
+
+food=input("Enter the Food Item : ")
+
+if food in indian:
+    print("Indian")
+elif food in south_indian:
+    print("South Indian")
+elif food in chinese:
+    print("Chinese")
+else:
+    print("FNF")"""
+
+
+# for loop
+
+exp=[13434,18470,19487,14786]
+total_expense=0
+
+for i in exp:
+    total_expense+=i
+
+print("Total Expense = ",total_expense)
+
+for i in range(1,11):
+    print(i**2)
+
+for i in range(len(exp)):
+    print("Month :",i+1," Expense :",exp[i])
+    total_expense+=exp[i]
+
+print("Total Expense :",total_expense)
+
+
+key_location="chair"
+locations=["table","bed","chair","microwave"]
+
+for i in locations:
+    if key_location == i:
+        print("Key Found on",i)
+        break
+    else:
+        print("Key Not Found on",i)
+
+for i in range(6):
+    if i%2 == 0:
+        continue
+    else:
+        print(i**2)
+
+# while statement
+
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+# function [function is a block of code that performs a specific task]
+
